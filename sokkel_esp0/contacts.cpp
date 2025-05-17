dc018755be187b3b4b99b26cd3d385588eb578a8
Thijs <t.kanters@uu.nl>
2025-05-17 14:38:09 +0200

    contacts done. but need testing

--- sokkel_esp0/contacts.cpp
+++ sokkel_esp0/contacts.cpp
@@ -6,14 +6,100 @@ Adafruit_MCP23X17 mcpABC;
 // GPIO expander for side D
 Adafruit_MCP23X08 mcpD;
 
-uint8_t Contacts::getPin(string connectorName){}
+
 
 void Contacts::setup(TwoWire& i2c){
     if(!mcpABC.begin_I2C(CONTACT_MCP_ABC_ADDR, &i2c) || !mcpD.begin_I2C(CONTACT_MCP_D_ADDR, &i2c)){
         Serial.println("[ERROR] - MCP I2C connection failed for contacts");
     }
-    setupPinMode()
+    setupPinMode();
     Serial.println("Setup contacts!");
+}
+
+bool Contacts::connected(enum ContactEnum c1, enum ContactEnum c2){
+  // Put 3.3v on c1
+  uint8_t c1_pin = getPin(c1);
+  if(isPlateD(c1)){
+    mcpD.pinMode(c1_pin, OUTPUT);
+    mcpD.digitalWrite(c1_pin, HIGH);
+  }
+  else{
+    mcpABC.pinMode(c1_pin, OUTPUT);
+    mcpABC.digitalWrite(c1_pin, HIGH);
+  }
+  // Test if c2 recieves power
+  uint8_t c2_pin = getPin(c2);
+  bool result;
+  if(isPlateD(c2)){
+    result = mcpD.digitalRead(c2_pin);
+  }
+  else{
+    result = mcpABC.digitalRead(c2_pin);
+  }
+  // reset c1
+  if(isPlateD(c1)){
+    mcpD.pinMode(c1_pin, INPUT_PULLDOWN);
+  }
+  return result;
+}
+
+
+bool Contacts::isPlateD(enum ContactEnum connectorName){
+  switch(connectorName){
+    case cD1:
+    case cD2:
+    case cD3:
+    case cD4:
+    case cD5:
+      return true;
+    default:
+      return false;
+  }
+}
+
+uint8_t Contacts::getPin(enum ContactEnum connectorName){
+    switch(connectorName){
+      case cA1:
+        return CONTACT_A1;
+      case cA2:
+        return CONTACT_A2;
+      case cA3:
+        return CONTACT_A3;
+      case cA4:
+        return CONTACT_A4;
+      case cA5:
+        return CONTACT_A5;
+      case cB1:
+        return CONTACT_B1;
+      case cB2:
+        return CONTACT_B2;
+      case cB3:
+        return CONTACT_B3;
+      case cB4:
+        return CONTACT_B4;
+      case cB5:
+        return CONTACT_B5;
+      case cC1:
+        return CONTACT_C1;
+      case cC2:
+        return CONTACT_C2;
+      case cC3:
+        return CONTACT_C3;
+      case cC4:
+        return CONTACT_C4;
+      case cC5:
+        return CONTACT_C5;
+      case cD1:
+        return CONTACT_D1;
+      case cD2:
+        return CONTACT_D2;
+      case cD3:
+        return CONTACT_D3;
+      case cD4:
+        return CONTACT_D4;
+      case cD5:
+        return CONTACT_D5;
+    }
 }
 
 void Contacts::setupPinMode(){
--- sokkel_esp0/contacts.h
+++ sokkel_esp0/contacts.h
@@ -7,13 +7,23 @@
 #include <Adafruit_MCP23X17.h>
 #include <Adafruit_MCP23X08.h>
 
+enum ContactEnum{
+  cA1, cA2, cA3, cA4, cA5,
+  cB1, cB2, cB3, cB4, cB5,
+  cC1, cC2, cC3, cC4, cC5,
+  cD1, cD2, cD3, cD4, cD5,
+};
+
 class Contacts{
     private:
       void setupPinMode();
       // Converts a connector name like "A2" or "D5" to a pinnumber
-      uint8_t getPin(string connectorName);
+      uint8_t getPin(enum ContactEnum connectorName);
+      bool isPlateD(enum ContactEnum connectorName);
     public:
       void setup(TwoWire& i2c);
-      bool connected()
+      bool connected(enum ContactEnum c1, enum ContactEnum c2);
 };
+
+
 #endif
